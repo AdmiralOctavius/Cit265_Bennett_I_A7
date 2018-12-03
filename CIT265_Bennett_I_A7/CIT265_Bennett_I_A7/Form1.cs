0b6770862645429cf41651f59434b8479713c164
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2018-12-03 13:40:13 -0500

    Start

---
+++ CIT265_Bennett_I_A7/CIT265_Bennett_I_A7/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CIT265_Bennett_I_A7
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            FolderBrowserDialog folderDialogue;
+
+            InitializeComponent();
+
+        }
+    }
+}
