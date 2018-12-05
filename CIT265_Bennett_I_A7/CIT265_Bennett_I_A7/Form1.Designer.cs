dfbefea0a57a3897688dbb068896edb42245c476
AdmiralOct <buildgamessnotbombs@gmail.com>
2018-12-05 01:47:59 -0500

    Finished Assignment

--- CIT265_Bennett_I_A7/CIT265_Bennett_I_A7/Form1.Designer.cs
+++ CIT265_Bennett_I_A7/CIT265_Bennett_I_A7/Form1.Designer.cs
@@ -30,59 +30,66 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
             // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Location = new System.Drawing.Point(12, 34);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(445, 277);
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(507, 368);
             this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(495, 90);
+            this.checkBox1.Location = new System.Drawing.Point(331, 8);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.Size = new System.Drawing.Size(108, 17);
             this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "View Sub-Folders";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(497, 118);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(495, 159);
+            this.textBox1.Location = new System.Drawing.Point(12, 8);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(249, 20);
             this.textBox1.TabIndex = 3;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(537, 446);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.listBox1);
+            this.MinimumSize = new System.Drawing.Size(553, 485);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "File Browser V1.0";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,8 +99,8 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- CIT265_Bennett_I_A7/CIT265_Bennett_I_A7/Form1.cs
+++ CIT265_Bennett_I_A7/CIT265_Bennett_I_A7/Form1.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*
+ * Name: Isaac Bennett
+ * Class: Cit265
+ * Professor: Davide Mauro
+ * Assignment: #7
+ * 
+ * 
+ * 
+ * */
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,8 +28,8 @@ namespace CIT265_Bennett_I_A7
 
         FolderBrowserDialog folderDialogue;
         public bool subfolderView = false;
-        //public int indexInMain=0;
         int selectionIndex = 0;
+
         public Form1()
         {
 
@@ -26,132 +37,112 @@ namespace CIT265_Bennett_I_A7
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+
+        //Main call for adding a new folder or file
+        private async void listBox1_DoubleClick(object sender, EventArgs e)
         {
 
+            string subDirectory = listBox1.SelectedItem.ToString();
+            selectionIndex = listBox1.SelectedIndex;
+
+            //Checks if it has to be recursive or not;
+            if(subfolderView == false)
+            {
+                //Add this sub folder's files to the listbox
+                string[] fileList = Directory.GetFiles(subDirectory);
+                foreach (var file in fileList)
+                {
+                    listBox1.Items.Insert(selectionIndex, file);
+                    selectionIndex++;
+                }
+                //The listbox1 handler for some reason becomes dominated by the above function, performing refresh fixes this.
+                listBox1.Refresh();
+
+                //Add this sub folder's files to the listbox
+                string[] directoryList = Directory.GetDirectories(subDirectory);
+
+                
+                foreach (var directory in directoryList)
+                {
+                    //This function call inserts a new string into the listbox's item list. This is used multiple times
+                    listBox1.Items.Insert(selectionIndex, directory);
+                }
+                listBox1.Refresh();
+            }
+            else
+            {
+                //Calls the start of the recursive display
+                await GetSubFolder(subDirectory);
+            }
         }
 
+        //Copy of the submit function to allow for better UX
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter)
             {
+
+                //Pull specified directory
                 string fileName = textBox1.Text;
+                //Resets listbox
+                listBox1.Items.Clear();
 
-                /*if (File.Exists(fileName)){
+                //If the directory exists at all
+                if (Directory.Exists(fileName))
+                {
 
-                    GetInformation(fileName);
-
-                    try
-                    {
-                        using(var stream = new StreamReader(fileName))
-                        {
-                            outputTextBox.AppendText(stream.ReadToEnd());
-                        }
-                    }
-                    catch (IOException)
-                    {
-                        MessageBox.Show("Error reading from file", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-
-                }*/
-                if (Directory.Exists(fileName)){
-                    //GetInformation(fileName);
-
+                    //Generate the list of items.
                     string[] directoryList = Directory.GetDirectories(fileName);
-                    //outputTextBox.AppendText("Directory contents:\n");
 
+                    //Add the starting folders to the listBox
                     foreach (var directory in directoryList)
                     {
-                        //outputTextBox.AppendText($"{directory}\n");
                         listBox1.Items.Add(directory);
                     }
 
                 }
+                //If directory does not exist
+
                 else
                 {
                     MessageBox.Show($"{textBox1.Text} does not exist", "File error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
-
-            
         }
 
-       /* private void GetInformation(string filename)
-        {
-            outputTextBox.Clear();
-
-            outputTextBox.AppendText($"{filename} exists\n" + Environment.NewLine);
-
-            outputTextBox.AppendText($"Created {File.GetCreationTime(filename)}\n" + Environment.NewLine);
-
-            outputTextBox.AppendText($"Last modified: {File.GetLastWriteTime(filename)}\n" + Environment.NewLine);
-            outputTextBox.AppendText($"Last accessed: {File.GetLastAccessTime(filename)}\n" + Environment.NewLine);
-        }
-        */
-        private async void listBox1_DoubleClick(object sender, EventArgs e)
-        {
-            string subDirectory = listBox1.SelectedItem.ToString();
-            selectionIndex = listBox1.SelectedIndex;
-            if(subfolderView == false)
-            {
-
-                
-                //outputTextBox.Clear();
-                
-                //outputTextBox.AppendText($"{subDirectory}");
-                
-                string[] directoryList = Directory.GetDirectories(subDirectory);
-                //outputTextBox.AppendText("Directory contents:\n");
-                
-                foreach (var directory in directoryList)
-                {
-                
-                    //outputTextBox.AppendText($"{directory}\n");
-                
-                    listBox1.Items.Insert(selectionIndex, directory);
-                }
-            }
-            else
-            {
-                await GetSubFolder(subDirectory);
-                
-            }
-        }
-
-        /*private void GetSubFolder(string inputFolder, ref int indexInMain)
-        {
-            
-
-            string[] directoryList = Directory.GetDirectories(inputFolder);
-            //outputTextBox.AppendText("Directory contents:\n");
-            foreach (var directory in directoryList)
-            {
-                indexInMain++;
-                //outputTextBox.AppendText($"{directory}\n");
-                listBox1.Items.Insert(indexInMain, directory);
-                GetSubFolder(directory, ref indexInMain);
-            }
-        }*/
-
+        //Recursive file and folder display function, largest function weight wise.
         private async Task GetSubFolder(string inputFolder)
         {
-           
-              
+            //Add this sub folder's files to the listbox
+            string[] fileList = Directory.GetFiles(inputFolder);
+            foreach (var file in fileList)
+            {
+                listBox1.Items.Insert(selectionIndex, file);
+                selectionIndex++;
+            }
+            //The listbox1 handler for some reason becomes dominated by the above function, performing refresh fixes this.
+            listBox1.Refresh();
+
+            //Adds the subfolders to the list
+            //This line gets a list of the directories
             string[] directoryList = Directory.GetDirectories(inputFolder);
-            //outputTextBox.AppendText("Directory contents:\n");
             foreach (var directory in directoryList)
             {
-                //outputTextBox.AppendText($"{directory}\n");
+                
                 listBox1.Items.Insert(selectionIndex, directory);
                 selectionIndex++;
+
+                //Calls the next sub folder asynchronously, allowing for the recursive feature
                 
-                await GetSubFolder(directory);
+                await Task.Run(()=> GetSubFolder(directory));
                 
                 
             }
             
+
         }
 
+        //Switches on and off the boolean for subfolderview (recursive display)
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             if(subfolderView == false)
@@ -163,5 +154,47 @@ namespace CIT265_Bennett_I_A7
                 subfolderView = false;
             }
         }
+
+        //Enter button for submitting the directory name
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Pull specified directory
+            string fileName = textBox1.Text;
+            //Resets listbox
+            listBox1.Items.Clear();
+
+            //If the directory exists at all
+            if (Directory.Exists(fileName))
+            {
+                
+                //Generate the list of items.
+                string[] directoryList = Directory.GetDirectories(fileName);
+                
+                //Add the starting folders to the listBox
+                foreach (var directory in directoryList)
+                {
+                    listBox1.Items.Add(directory);
+                }
+
+            }
+            //If directory does not exist
+
+            else
+            {
+                MessageBox.Show($"{textBox1.Text} does not exist", "File error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
